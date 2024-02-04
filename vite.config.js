05ddd18123b8b00b2f5693fac9cfaf76c6c202d6
aryanc193 <aryanc193@gmail.com>
2024-02-04 12:54:02 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Specify the output directory for the build
+    outDir: 'dist',
+  },
 })
