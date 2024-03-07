05620143b8336f10cbfc62efa86df8e9e5bfe82d
aryanc193 <aryanc193@gmail.com>
2024-03-07 21:53:37 +0530

    modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <div className="website-visit-counter">website views: 244</div>
+        <div className="website-visit-counter">website views: 369</div>
         <LikeButton />
         <div className="contact-div">
           <ul className="sci">
